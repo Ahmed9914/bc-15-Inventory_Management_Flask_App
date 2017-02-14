782d382f631853b39eb2eca89d668b24a2ad0c1d
Jxn <jackson.onyango@gmail.com>
2017-02-14 20:38:12 +0300

    create admin action pager where an admin user can choose from a list of actions to do(add asset, assign/unassign asset, view a list, add a user

--- views.py
+++ views.py
@@ -13,7 +13,7 @@ def super_admin_login():
     form = LoginForm()
     if form.validate_on_submit():
         username = form.username.data
-        flash("Signed in successfully as {}".format(username))
+        flash("Signed in successfully as {} <SuperAdmin User>".format(username))
         return redirect(request.args.get('next') or url_for('super_admin'))
     return render_template('super_admin_login.html', form = form)
 
@@ -23,8 +23,8 @@ def admin_login():
     form = LoginForm()
     if form.validate_on_submit():
         username = form.username.data
-        flash("Signed in successfully as {}".format(username))
-        return redirect(request.args.get('next') or url_for('home'))
+        flash("Signed in successfully as {}  <Admin User>".format(username))
+        return redirect(request.args.get('next') or url_for('admin'))
     return render_template('admin_login.html', form = form)
 
 
@@ -33,7 +33,7 @@ def user_login():
     form = LoginForm()
     if form.validate_on_submit():
         username = form.username.data
-        flash("Signed in successfully as {}".format(username))
+        flash("Signed in successfully as {} <User>".format(username))
         return redirect(request.args.get('next') or url_for('home'))
     return render_template('user_login.html', form = form)
 
@@ -49,4 +49,8 @@ def super_admin():
         flash("Successfully created {} as Admin".format(username))
         return redirect(request.args.get('next') or url_for('super_admin'))
     return render_template('super_admin.html', form = form)
+
+@app.route('/admin')
+def admin():
+    return render_template("admin.html")
 
