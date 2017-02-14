ff8b4731c5e48444070e8814596197811aa829c6
Jxn <jackson.onyango@gmail.com>
2017-02-14 15:37:16 +0300

    Fix Super admin login link

--- forms.py
+++ forms.py
@@ -3,6 +3,6 @@ from wtforms.fields import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired
 
 class LoginForm(Form):
-    username = StringField('Username:', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()] )
-    submit = SubmitField('Sign In')
+    username = StringField('Username: ', validators=[DataRequired()])
+    password = PasswordField('Password: ', validators=[DataRequired()] )
+    submit = SubmitField('Sign In: ')
--- views.py
+++ views.py
@@ -10,9 +10,8 @@ def home():
 @app.route('/super_admin_login', methods = ["GET", "POST"])
 def super_admin_login():
     form = LoginForm()
-    if form.validate_on_submit:
+    if form.validate_on_submit():
         username = form.username.data
-        flash("Logged in successfully as {}".format(username))
+        flash("Signed in successfully as {}".format(username))
         return redirect(request.args.get('next') or url_for('home'))
-    flash('Incorrect username or password.')
     return render_template('super_admin_login.html', form = form)
