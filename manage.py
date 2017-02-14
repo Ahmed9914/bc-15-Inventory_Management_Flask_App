896c05d85650a62d85514bf3cce0c5985f3b2aae
Jxn <jackson.onyango@gmail.com>
2017-02-14 16:53:38 +0300

    Modify manage.py to create and drop db

--- manage.py
+++ manage.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python3
-from __init__ import app
-from flask.ext.script import Manager
+from __init__ import app, db
+from flask.ext.script import Manager, prompt_bool
 
 #assign manager variable to Manager function with app passesd to it
 manager = Manager(app)
+
+@manager.command
+def initdb():
+    db.create_all()
+    db.session.commit()
+    print('Successfully created the database')
+
+@manager.command
+def dropdb():
+    if prompt_bool("Are you sure you want to drop the database?"):
+        db.drop_all()
+        print("Successfully dropped the database")
+
+import models
 
 if __name__ == "__main__":
     manager.run()
