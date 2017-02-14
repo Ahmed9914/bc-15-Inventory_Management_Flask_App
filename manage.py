5c4f8eac38b62cbd38f98693f5d1085118ee9c4d
Jxn <jackson.onyango@gmail.com>
2017-02-14 08:45:57 +0300

    Correct __init__.py to __init__ in manage.py import statement

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from __init__.py import app
+from __init__ import app
 from flask.ext.script import Manager
 
 #assign manager variable to Manager function with app passesd to it
