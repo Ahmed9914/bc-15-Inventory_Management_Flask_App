7b131409b3af0ad443c30a15f10e2a15b7ec4a7d
Jxn <jackson.onyango@gmail.com>
2017-02-17 00:24:18 +0300

    Change wording on button labels

--- forms.py
+++ forms.py
@@ -5,15 +5,15 @@ from wtforms.validators import DataRequired
 class LoginForm(Form):
     username = StringField('Username: ', validators=[DataRequired()])
     password = PasswordField('Password: ', validators=[DataRequired()] )
-    submit = SubmitField('Sign In: ')
-    create = SubmitField('Create Account: ')
+    submit = SubmitField('Sign In ')
+    create = SubmitField('Create Account ')
 
 class UserForm(Form):
     username = StringField('Username: ', validators=[DataRequired()])
     first_name = StringField('First Name: ')
     last_name = StringField('Last Name: ')
-    submit = SubmitField('Sign In: ')
-    create = SubmitField('Create Account: ')
+    submit = SubmitField('Sign In ')
+    create = SubmitField('Create Account ')
 
 class AssetForm(Form):
     asset_name = StringField('Asset name: ', validators=[DataRequired()])
@@ -22,24 +22,24 @@ class AssetForm(Form):
     serial_code = StringField('Serial Code: ', validators=[DataRequired()])
     date_bought = StringField('Bought on: ', validators=[DataRequired()])
     colour = StringField('Color(optional): ')    
-    add = SubmitField('Add asset: ')
+    add = SubmitField('Add asset ')
     reclaim_date = DateField('Reclaim Date:')
     
 class AssignForm(Form):
     asset_name = StringField('Asset name: ', validators=[DataRequired()])
     user_assigned = StringField('User to be assigned: ')
     reclaim_date = DateField('Reclaim Date:')
-    assign = SubmitField('Assign asset: ')
-    unassign = SubmitField('Unassign asset: ')
+    assign = SubmitField('Assign asset ')
+    unassign = SubmitField('Unassign asset ')
 
 class CaseForm(Form):
     asset_name = StringField('Asset Name: ', validators=[DataRequired()])
     serial_num = StringField('Serial Number: ', validators=[DataRequired()])
-    report_lost = SubmitField('Report As Lost: ')
-    report_found = SubmitField('Report As Found: ')
+    report_lost = SubmitField('Report As Lost ')
+    report_found = SubmitField('Report As Found ')
 
 class ResolveForm(Form):
     text = StringField('txt')
-    resolve = SubmitField('Resolve Case: ')
+    resolve = SubmitField('Resolve Case ')
 
 
